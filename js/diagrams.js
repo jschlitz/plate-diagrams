021452bd4221dae2f7e5150ee23e1f7a0c8ee177
Jacob Schlitz <jschlitz@gmail.com>
2015-06-06 18:06:49 -0400

    jquery-ifying it.

--- js/diagrams.js
+++ js/diagrams.js
@@ -2,6 +2,43 @@ $(document).ready(function(){
   var logged = "";
   var LOREM = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
 
+  var makeDiagram = function(items) {
+    var result = $('<div>').attr('class', 'container');
+    for (var i = 0; i < items.length; i++) {
+      result.append(items[i]);
+    }
+
+    return result;
+  };
+
+  var makeSource = function(text, count){
+    var result = $('<div>').attr('class', 'subcontainer');
+    result.append($('<div>').attr('class', 'source').text(count));
+    result.append($('<p>').text(text));
+
+    return result;
+  };
+
+  var makeArrow = function(){
+    return $('<div>').attr('class', 'arrow');
+  };
+
+  var makeDestination = function (text, wells, startColor) {
+    var result = $('<div>').attr('class', 'subcontainer');
+    result.append($('<div>').attr('class', 'plate2'));
+    result.append($('<p>').text(text));
+
+    return result;
+  }
+
+  var main = $("#main");
+  main.empty();
+
+  main.append(makeDiagram([makeSource("Hello world", 96),
+    makeArrow(),
+    makeDestination("wibble wobble", [8,7,6,5,4,3,2,1], 0)]));
+
+
 
   //$("#content").text("Hello World!")
 

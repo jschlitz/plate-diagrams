7b5ec93987ea90b9105a8de3fdf3996b265e8000
Jacob Schlitz <jschlitz@gmail.com>
2015-06-06 16:08:57 -0400

    first pass.

---
+++ js/diagrams.js
@@ -0,0 +1,38 @@
+$(document).ready(function(){
+  var logged = "";
+  var LOREM = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+
+
+  //$("#content").text("Hello World!")
+
+/*
+
+
+ $("#about").on("click", function (e){
+    $("#content").empty();
+    $("#content").append($("<p>").text("foo"));
+    $("#content").append($("<br>"));
+    $("#content").append($("<p>").text(LOREM));
+  });
+
+  $("#home").on("click", function (e){
+    $("#content").empty();
+    $("#content").append($("<p>").text("Welcome home."));
+  });
+
+  $("#menu").on("click", function (e){
+    $("#content").empty();
+    var stuff = $("<div>");
+    var img = $("<img>");
+    img.attr("src", "foxhead.png");
+    stuff.append(img);
+    stuff.append($("<p>").text("buy this!"));
+
+    $("#content").append(stuff);
+    $("#content").append($("<br>"));
+    $("#content").append(stuff.clone());
+    $("#content").append($("<br>"));
+    $("#content").append(stuff.clone());
+  });
+*/
+});
